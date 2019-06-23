cdfaecbeb87821db5e3c5ad1a15a441e90dc0441
Elophe <olivier.elophe@gmail.com>
2019-06-23 19:09:20 +0200

    Commentaires détailés de la solution à la partie 1

--- Partie1_Solution/js/Favoris.js
+++ Partie1_Solution/js/Favoris.js
@@ -26,37 +26,39 @@ let listeLiens = [
 // Crée et renvoie un élément DOM affichant les données d'un lien
 // Le paramètre lien est un objet JS représentant un lien
 function creerElementLien(lien) {
-    var titreLien = document.createElement("a");
-    titreLien.href = lien.url;
-    titreLien.style.color = "#FF8C00";
-    titreLien.style.textDecoration = "none";
-    titreLien.style.marginRight = "5px";
-    titreLien.appendChild(document.createTextNode(lien.titre));
+    // On commence par créer une balise <a> qui va contenir le titre du lien cliquable.
+    var titreLien = document.createElement("a"); // Création d'un élément <a>
+    titreLien.href = lien.url; // Ajout du href à la balise <a> crée précédemment
+    titreLien.style.color = "#FF8C00"; // Modification du style de la balise <a>
+    titreLien.style.textDecoration = "none"; // Modification du style de la balise <a>
+    titreLien.style.marginRight = "5px"; // Modification du style de la balise <a>
+    titreLien.appendChild(document.createTextNode(lien.titre)); // On rajoute à la balise <a> un fils de type texte qui contient le titre du lien
 
-    var urlLien = document.createElement("span");
-    urlLien.appendChild(document.createTextNode(lien.url));
+    var urlLien = document.createElement("span"); // Création d'un élément <span>
+    urlLien.appendChild(document.createTextNode(lien.url)); // On rajoute à la balise <span> un fils de type texte qui contient l'adresse du lien
 
     // Cette ligne contient le titre et l'URL du lien
-    var ligneTitre = document.createElement("h4");
-    ligneTitre.style.margin = "0px";
-    ligneTitre.appendChild(titreLien);
-    ligneTitre.appendChild(urlLien);
+    var ligneTitre = document.createElement("h4"); // Création d'un élément <h4>
+    ligneTitre.style.margin = "0px"; // Modification du style de la balise <h4>
+    ligneTitre.appendChild(titreLien); // On ajoute à la balise <h4> le fils <a> précédent
+    ligneTitre.appendChild(urlLien); // Puis on ajoute à la balise <h4> le fils <span> précédent
 
     // Cette ligne contient l'auteur
-    var ligneDetails = document.createElement("span");
-    ligneDetails.appendChild(document.createTextNode("Présenté par " + lien.auteur));
+    var ligneDetails = document.createElement("span"); // Création d'un élément <span>
+    ligneDetails.appendChild(document.createTextNode("Présenté par " + lien.auteur)); // On ajoute un noeud text à cette balise <span>
 
-    var divLien = document.createElement("div");
-    divLien.classList.add("lien");
-    divLien.appendChild(ligneTitre);
-    divLien.appendChild(ligneDetails);
+    var divLien = document.createElement("div"); // Création d'un élément <div>
+    divLien.classList.add("lien"); // On rajoute le nom de class lien à la div, pour qu'elle soit ciblée par le selecteur css .lien
+    divLien.appendChild(ligneTitre); // On rajoute dans cette div le <h4> précédent
+    divLien.appendChild(ligneDetails); // Puis on rajoute dans cette div le <span> précédent
 
-    return divLien;
+    return divLien; // La fonction renvoit cette <div>
 }
 
-var contenu = document.getElementById("contenu");
+var contenu = document.getElementById("contenu"); // On selectionne notre div d'Id contenu
+
 // Parcours de la liste des liens et ajout d'un élément au DOM pour chaque lien
-listeLiens.forEach(function (lien) {
+listeLiens.forEach(function (lien) {  //forEach permet d'applique la fonction à chaque élément d'un tableau
     var elementLien = creerElementLien(lien);
-    contenu.appendChild(elementLien);
+    contenu.appendChild(elementLien); // On ajoute notre div renvoyée par la fonction à notre div contenu.
 });
